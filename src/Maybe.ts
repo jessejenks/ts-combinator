3eaed8ef9774dcfb7b3e6bfdeb6b582dcd15fedc
Jesse Jenks <JesseEndoJenks@gmail.com>
2026-02-23 17:19:59 +0900

    Adds more helper functions for Maybe and Result

--- src/Maybe.ts
+++ src/Maybe.ts
@@ -38,4 +38,46 @@ export namespace Maybe {
     export function isNothing<T>(maybe: Maybe<T>): maybe is Nothing {
         return maybe.variant === Variant.Nothing;
     }
+
+    export const map = <A, B>(f: (a: A) => B, maybe: Maybe<A>): Maybe<B> => {
+        switch (maybe.variant) {
+            case Variant.Nothing:
+                return maybe;
+
+            case Variant.Just:
+                return Maybe.Just(f(maybe.value));
+        }
+    };
+
+    export const bind = <A, B>(
+        f: (a: A) => Maybe<B>,
+        maybe: Maybe<A>,
+    ): Maybe<B> => {
+        switch (maybe.variant) {
+            case Variant.Nothing:
+                return maybe;
+
+            case Variant.Just:
+                return f(maybe.value);
+        }
+    };
+
+    export const flatMap = bind;
+
+    export const chain = bind;
+
+    export const all = <T>(maybes: Maybe<T>[]): Maybe<T[]> => {
+        const innerMaybes = new Array<T>(maybes.length);
+        for (let i = 0; i < maybes.length; i++) {
+            const result = maybes[i];
+            if (Maybe.isNothing(result)) {
+                return result;
+            }
+            innerMaybes[i] = result.value;
+        }
+        return Maybe.Just(innerMaybes);
+    };
+
+    export const unwrapOr = <T>(maybe: Maybe<T>, fallback: T): T =>
+        Maybe.isNothing(maybe) ? fallback : maybe.value;
 }
--- src/Result.ts
+++ src/Result.ts
@@ -42,6 +42,14 @@ export namespace Result {
         return result.variant === Variant.Err;
     }
 
+    export const fromNullable = <T, E>(
+        value: T | null | undefined,
+        err: E,
+    ): Result<T, E> =>
+        value === null || value === undefined
+            ? Result.Err(err)
+            : Result.Ok(value);
+
     /**
      * Takes the value from an Ok result and transforms it based on the given function.
      * Acts like identity function in Err case
@@ -60,4 +68,54 @@ export namespace Result {
                 return Result.Ok(f(result.value));
         }
     };
+
+    export const mapErr = <T, E, F>(
+        f: (a: E) => F,
+        result: Result<T, E>,
+    ): Result<T, F> => {
+        switch (result.variant) {
+            case Variant.Err:
+                return Result.Err(f(result.error));
+
+            case Variant.Ok:
+                return result;
+        }
+    };
+
+    export const bind = <A, B, E>(
+        f: (a: A) => Result<B, E>,
+        result: Result<A, E>,
+    ): Result<B, E> => {
+        switch (result.variant) {
+            case Variant.Err:
+                return result;
+
+            case Variant.Ok:
+                return f(result.value);
+        }
+    };
+
+    export const flatMap = bind;
+
+    export const chain = bind;
+
+    export const all = <T, E>(results: Result<T, E>[]): Result<T[], E> => {
+        const innerResults = new Array<T>(results.length);
+        for (let i = 0; i < results.length; i++) {
+            const result = results[i];
+            if (Result.isErr(result)) {
+                return result;
+            }
+            innerResults[i] = result.value;
+        }
+        return Result.Ok(innerResults);
+    };
+
+    export const unwrapOr = <T, E>(result: Result<T, E>, fallback: T): T =>
+        Result.isErr(result) ? fallback : result.value;
+
+    export const unwrapOrElse = <T, E>(
+        result: Result<T, E>,
+        fallback: (e: E) => T,
+    ): T => (Result.isErr(result) ? fallback(result.error) : result.value);
 }
